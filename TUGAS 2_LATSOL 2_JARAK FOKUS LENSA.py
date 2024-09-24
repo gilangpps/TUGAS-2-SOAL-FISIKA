88395051e3dff8e070a41033249c250ab29c585e
gilangpps <gilangpputras03@gmail.com>
2024-09-24 08:37:23 +0700

    Add files via upload

---
+++ TUGAS 2_LATSOL 2_JARAK FOKUS LENSA.py
@@ -0,0 +1,18 @@
+#SOAL 1
+#JARAK FOKUS LENSA
+
+#rumus
+# 1/f = (n-1)*((1/R1)+(1/R2))
+
+n = 1.50
+R1 = 22 #cm
+R2 = 17.5 #cm
+
+f_awal = (n-1)*((1/R1)+(1/R2))
+f = 1/f_awal
+
+print("Jarak Fokus Lensa - Praktikum Fisika Komputasi")
+print("=======================================================")
+print("Oleh : Gilang Pratama Putra Siswanto (NIM 1227030017)")
+print(" ")
+print("Jarak fokus :", f, "cm")
